a3f8075ed82bcec9680f9dc5ac7214d6fe692025
Natan Sanches <natanhsanches@gmail.com>
2023-06-18 21:59:59 -0300

    Add base colors as const variables

--- include/Color.h
+++ include/Color.h
@@ -2,8 +2,15 @@
 
 #include "Vec3.hpp"
 
-namespace Color {
+namespace color {
     using Color = Vec3;
 
-    void write(std::ostream&, Color&);
+    const auto Red = Color(1, 0, 0);
+    const auto Green = Color(0, 1, 0);
+    const auto Blue = Color(0, 0, 1);
+    const auto White = Color(1, 1, 1);
+    const auto Black = Color(0, 0, 0);
+    const auto Gray = Color(0.5, 0.5, 0.5);
+
+    void write(std::ostream&, const Color&);
 }
